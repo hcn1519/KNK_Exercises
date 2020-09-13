b41608873dcd8456c04324c3e12fb03b416658ac
Changnam Hong <hcn1519@gmail.com>
2020-09-14 03:07:16 +0900

    gcd sample

--- KNK_CH9_No6/KNK_CH9_No6/main.c
+++ KNK_CH9_No6/KNK_CH9_No6/main.c
@@ -8,6 +8,22 @@
 
 #include <stdio.h>
 
+int gcd(int a, int b) {
+    int tmp;
+    if (b > a) {
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    int mod = a % b;
+
+    if (mod == 0) {
+        return b;
+    } else {
+        return gcd(a % b, b);
+    }
+}
+
 int digit(int n, int k) {
     if (k == 1) {
         return n % 10;
@@ -26,5 +42,15 @@ int main(int argc, const char * argv[]) {
     printf("Result2 - %d\n", result2);
     printf("Result3 - %d\n", result3);
     printf("Result4 - %d\n", result4);
+
+    int gcd1 = gcd(12 , 4);
+    int gcd2 = gcd(40 , 22);
+    int gcd3 = gcd(888 , 220);
+    int gcd4 = gcd(121 , 33);
+
+    printf("GCD1 - %d\n", gcd1);
+    printf("GCD2 - %d\n", gcd2);
+    printf("GCD3 - %d\n", gcd3);
+    printf("GCD4 - %d\n", gcd4);
     return 0;
 }
